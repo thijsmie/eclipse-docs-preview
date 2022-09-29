d4bdaf12c1617df33a3aaeef7862d3faa3d1e4af
Thijs Miedema <mail@tmiedema.com>
2022-09-29 15:05:35 +0200

    Links for docu.js

--- pages/js/docu.js
+++ pages/js/docu.js
@@ -1,10 +1,17 @@
 window.addEventListener("load", function() {
     let e = document.querySelector(".sphinxsidebarwrapper");
     let ul = document.createElement("ul");
-    let version = window.location.href.split('/').slice(-2)[0];
+    let path = window.location.href.split('/');
+    var root;
+
+    for(var i = 0; i < path.length; ++i) {
+        if (path[i] == "docs") {
+            root = path.slice(0, i+1).join('/')
+        }
+    }
+
     ul.innerHTML = `
-        <li class="toctree-l1"><a class="reference internal">Version: <strong>${version}</strong></a></li>
-        <li class="toctree-l1"><a class="reference internal" href="/docs">Back to overview</a></li>
+        <li class="toctree-l1"><a class="reference internal" href="${root}/index.html">Back to overview</a></li>
     `;
     e.insertBefore(ul, e.firstChild);
     Array(...document.querySelectorAll("dt.py")).forEach((e) => e.classList.add("highlight"));
